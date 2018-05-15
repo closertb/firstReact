76df4f9c50b19976ac96e0467a2a4384be5c2061
botang <bo.tang3@56qq.com>
2018-05-15 23:14:53 +0800

    async generator learning

--- PickerComponent/src/js/index.js
+++ PickerComponent/src/js/index.js
@@ -51,7 +51,8 @@ function task(iterator) {
   const iter = iterator();
   console.log(iter);
   function next(args) {
-    const result = iter.next(args);
+    const result = iter.next(args); // { value: { type: 'take' }, done: false }
+    console.log(result);
     if (!result.done) {
       const effect = result.value;
       if (effect.type === 'take') {
@@ -60,13 +61,63 @@ function task(iterator) {
       }
     }
   }
-  next();
+  next({ type: 'taker' });
 }
 function* mainSaga() {
-  const action = yield take();
+  const action = yield take(); // { type: take }
   console.log(action);
 }
 task(mainSaga);
+
+/* function* itera() {
+  const fir = yield 1;
+  console.log('in', fir);
+  const sec = yield fir + 2;
+  yield sec + 3;
+}
+const iter = itera();
+console.log('fir', iter.next());
+console.log('sec', iter.next(3));
+console.log('thr', iter.next(4)); */
+
 /* render((
     <Treat />
 ), document.getElementById('app')); */
+
+function fetchData() {
+  return (callback) => {
+    setTimeout(() => {
+      callback(null, 'HI');
+    }, 500);
+  };
+}
+
+function run(taskDef) {
+  const task = taskDef();
+  let result = task.next();
+  function step() {
+    if (!result.done) {
+      if (typeof result.value === 'function') {
+        result.value((err, data) => {
+          if (err) {
+            result = task.throw(err);
+            return;
+          }
+          result = task.next(data);
+          step();
+        });
+      } else {
+        result = task.next(result.value);
+        step();
+      }
+    }
+  }
+  step();
+}
+
+run(function* unname() {
+  const contents = yield fetchData();
+  console.log(contents);
+  const op = yield 3;
+  console.log(op);
+});
